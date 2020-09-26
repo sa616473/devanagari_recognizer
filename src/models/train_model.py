a43aed9ec0084c59faef27ab602e72207bcb182c
sa616473 <saitejasmopuri@gmail.com>
2020-09-26 00:26:28 -0400

    Adding graphs to the notebook

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,4 +1,3 @@
-
 from tensorflow.keras.models import model_from_json
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Dense, Flatten,Conv2D, MaxPool2D, Dropout
@@ -15,6 +14,10 @@ import numpy as np
 
 
 def save_model(model, title=''):
+    '''
+    This function saves the model into HDF5 format and weights
+    into a JSON file
+    '''
     model_json = model.to_json()
     with open("../src/models/saved_models/model_{}.json".format(title), "w") as json_file:
         json_file.write(model_json)
@@ -22,32 +25,53 @@ def save_model(model, title=''):
     model.save_weights("../src/models/saved_models/model_{}.h5".format(title))
     print("Saved model to disk")
 
-#1st model simple DNN
-def dnn_model(training=(), val=(), test=(), epochs = 100, layers=[1024, 512, 128]):
-    performance = {}
-    
+def callbacks():
+    '''
+    Early stopping callbacks
+    '''
     callback = EarlyStopping(
     monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='auto',
     baseline=None, restore_best_weights=True)
+    return callback
+
+#1st model simple DNN
+def dnn_model(training=(), val=(), test=(), epochs = 100, layers=[1024, 512, 128]):
+    #Performance dictionary
+    performance = {}
+    
+    #Earlystopping
+    callback = callbacks()
     
     i=0
+    
     for layer in layers:
+        #model
         model = Sequential()
         model.add(Flatten(input_shape=(32,32)))
         model.add(Dense(layer, activation='relu'))
         model.add(Dense(46, activation='softmax'))
-
+        
+        #Compliling the model
         model.compile(optimizer='adam', 
                       loss=SparseCategoricalCrossentropy(from_logits=True), 
                       metrics=['accuracy'])
-
+        
+        #Training the model
         history = model.fit(training[0], training[1],
                   epochs=epochs,
                   callbacks=[callback],
-                 validation_data=(val[0], val[1]))
+                 validation_data=(val[0], val[1]), verbose=3)
+        
+        #Visualizing the model
         vs.training_visualize(history, title='DNN {} layer'.format(layer))
+        
+        #Save model
         save_model(model, title='{}'.format(layer))
+        
+        #Evaluate model
         evaluation = model.evaluate(test[0], test[1])
+        
+        #Record Performance.
         performance[i] = [evaluation[0], evaluation[1], layer]   
         i = i+1
     performance = pd.DataFrame(data=performance)
@@ -57,8 +81,7 @@ def dnn_model(training=(), val=(), test=(), epochs = 100, layers=[1024, 512, 128
     
     return performance
 
-def cnn_model(train=(), test=(), val=(),
-              epochs = 100, layers=[128,64,32]):
+def cnn_model(train=(), test=(), val=(),epochs = 100, layers=[128,64,32]):
     performance = {}
     
     checkpoint_filepath = '../src/models/checkpoints'
